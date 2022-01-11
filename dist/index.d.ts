4e16251684cf65ad0b9c9450e70d5ef7d55d655a
Stanley Jones <stanley@sunshocked.com>
2022-01-10 20:02:54 -0800

    Add keys to index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,10 @@
 import * as identity from "./identity";
+import * as keys from "./keys";
 import * as message from "./message";
 import * as server from "./server";
 declare const omni: {
     identity: typeof identity;
+    keys: typeof keys;
     message: typeof message;
     server: typeof server;
 };
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 import * as identity from "./identity";
+import * as keys from "./keys";
 import * as message from "./message";
 import * as server from "./server";
-const omni = { identity, message, server };
+const omni = { identity, keys, message, server };
 export default omni;
