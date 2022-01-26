ea3069d2c1a55f54e48021d217984081e78a4d7b
oneway <trust.biz888@gmail.com>
2022-01-26 13:25:42 -0600

    fix base32encode with padding

--- src/identity.test.ts
+++ src/identity.test.ts
@@ -13,7 +13,40 @@ describe("identity", () => {
   test("fromPublicKey", () => {
     expect(identity.fromPublicKey(publicKey)).toStrictEqual(id);
   });
+  
+  test("fromString", () => {
+    expect(identity.fromString(stringId)).toStrictEqual(id);
+  });
 
+  test("toString", () => {
+    expect(identity.toString(id)).toStrictEqual(stringId);
+  });
+
+  test("fromHex", () => {
+    expect(identity.fromHex(hexId)).toStrictEqual(id);
+  });
+
+  test("toHex", () => {
+    expect(identity.toHex(id)).toStrictEqual(hexId);
+  });
+});
+
+describe("identity 2", () => {
+  const publicKey = Buffer.from([
+    212, 98, 187, 130, 193, 168, 253, 87, 174, 146, 90, 55, 239, 156, 57, 72,
+    124, 116, 29, 162, 239, 193, 191, 6, 124, 134, 68, 104, 182, 57, 17, 196,
+  ]);
+
+  //const id = identity.fromPublicKey(publicKey);
+    const hexId = "804a101d521d810211a0c6346ba89bd1cc1f821c03b969ff9d5c8b2f59123456";
+  const id = identity.fromHex(hexId);
+  const stringId = "oqbfbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wisgrlakw";
+  /*
+  test("fromPublicKey", () => {
+    expect(identity.fromPublicKey(publicKey)).toStrictEqual(id);
+  });
+  */
+  
   test("fromString", () => {
     expect(identity.fromString(stringId)).toStrictEqual(id);
   });
--- src/identity.ts
+++ src/identity.ts
@@ -29,7 +29,7 @@ export function toString(identity?: Identity): string {
   checksum.writeUInt16BE(crc.crc16(identity), 0);
 
   const leader = "o";
-  const base32Identity = base32Encode(identity, "RFC4648").slice(0, -1);
+  const base32Identity = base32Encode(identity, "RFC4648", {padding: false});
   const base32Checksum = base32Encode(checksum, "RFC4648").slice(0, 2);
   return (leader + base32Identity + base32Checksum).toLowerCase();
 }
