23f67318cbf32a1378a6aa3ac4773c5c8826c757
Stanley Jones <stanley@sunshocked.com>
2022-01-26 14:38:49 -0800

    Build dist on install

--- dist/identity.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { Key } from "./keys";
-export declare type Identity = Buffer;
-export declare function fromBuffer(buffer: Uint8Array): Identity;
-export declare function fromPublicKey(key: Key): Identity;
-export declare function fromString(string: string): Identity;
-export declare function toString(identity?: Identity): string;
-export declare function fromHex(hex: string): Identity;
-export declare function toHex(identity?: Identity): string;
