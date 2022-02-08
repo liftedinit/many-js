1926c677fb8f04f05dbc627aa999a2968e018661
Stanley Jones <stanley@sunshocked.com>
2022-02-08 14:54:13 -0800

    fix: remove comments

--- src/identity.test.ts
+++ src/identity.test.ts
@@ -1,29 +1,17 @@
 import * as identity from "./identity";
 
 describe("identity", () => {
-  // const publicKey = Buffer.from([
-  //   212, 98, 187, 130, 193, 168, 253, 87, 174, 146, 90, 55, 239, 156, 57, 72,
-  //   124, 116, 29, 162, 239, 193, 191, 6, 124, 134, 68, 104, 182, 57, 17, 196,
-  // ]);
-  //
-  // const id = identity.fromPublicKey(publicKey);
-  // const hexId = "016ec855dc20ba87759f24bfc123491a50a3f93cacbe0cb5636b9bb95e";
-  // const stringId = "oafxmqvo4ec5io5m7es74ci2jdjikh6j4vs7aznldnon3sxqt6";
-
-  const id = (seed: number) => {
+  function id(seed: number) {
     // prettier-ignore
     const bytes = new Uint8Array([
       1,
       0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0,
-      seed >> 24,
-      seed >> 16,
-      seed >> 8,
-      seed & 0xff,
+      seed >> 24, seed >> 16, seed >> 8, seed & 0xff,
     ]);
     return identity.fromBuffer(bytes);
-  };
+  }
 
   test("can read anonymous", () => {
     const anon = identity.anonymous();
