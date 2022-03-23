bb24b936b91d53902a81dd5ed51fd6ff2813da54
Tommy <tommy@l-1.us>
2022-03-23 11:45:02 -0700

    fix mock for symbol identities

--- src/network/modules/ledger/data.ts
+++ src/network/modules/ledger/data.ts
@@ -1,34 +1,18 @@
+import { Identity } from "../../../identity";
 import { tag } from "../../../message/cbor";
 
-export const mockSymbolIdentity = [
-  tag(
-    10000,
-    // todo: find out how to mock the symbol Identity
-    // oqbfbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wiaaaaqnz
-    Buffer.from([
-      128, 74, 16, 29, 82, 29, 129, 2, 17, 160, 198, 52, 107, 168, 155, 209,
-      204, 31, 130, 28, 3, 185, 105, 255, 157, 92, 139, 47, 89, 0, 0, 1,
-    ])
-  ),
-  "abc",
-];
+const identityStr1 = "oqbfbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wiaaaaqnz";
+const Identity1 = Identity.fromString(identityStr1).toBuffer();
+const identityStr2 = "oaffbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wijp";
+const Identity2 = Identity.fromString(identityStr2).toBuffer();
 
-export const mockSymbolIdentity2 = [
-  // todo: find out how to mock the symbol Identity
-  // "oaffbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wijp"
-  tag(
-    10000,
-    Buffer.from([
-      1, 74, 16, 29, 82, 29, 129, 2, 17, 160, 198, 52, 107, 168, 155, 209, 204,
-      31, 130, 28, 3, 185, 105, 255, 157, 92, 139, 47, 89,
-    ])
-  ),
-  "cba",
-];
+export const mockSymbolIdentity = [tag(10000, Identity1), "abc"];
+
+export const mockSymbolIdentity2 = [tag(10000, Identity2), "cba"];
 
 export const expectedSymbolsMap = {
   symbols: new Map([
-    ["oaffbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wijp", "cba"],
-    ["oqbfbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wiaaaaqnz", "abc"],
+    [identityStr2, "cba"],
+    [identityStr1, "abc"],
   ]),
 };
