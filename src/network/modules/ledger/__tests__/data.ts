e530d71ee7b20e5dbce596052c8722487c4595dc
Tommy Tan <tantommy@users.noreply.github.com>
2022-03-31 09:14:28 -0700

    adds tests for ledger.balance (#18)

--- src/network/modules/ledger/__tests__/data.ts
+++ src/network/modules/ledger/__tests__/data.ts
@@ -1,5 +1,7 @@
 import { Identity } from "../../../../identity"
+import cbor from "cbor"
 import { tag } from "../../../../message/cbor"
+import { Message } from "../../../../message"
 
 const identityStr1 = "oqbfbahksdwaqeenayy2gxke32hgb7aq4ao4wt745lsfs6wiaaaaqnz"
 const Identity1 = Identity.fromString(identityStr1).toBuffer()
@@ -14,5 +16,51 @@ export const expectedSymbolsMap = {
   symbols: new Map([
     [identityStr2, "cba"],
     [identityStr1, "abc"],
+  ]),
+}
+
+export const mockLedgerInfoResponseContent = new Map([
+  [
+    4,
+    cbor.encode(
+      new Map([
+        [
+          4,
+          // @ts-ignore
+          new Map([mockSymbolIdentity, mockSymbolIdentity2]),
+        ],
+      ]),
+    ),
+  ],
+])
+
+export const mockLedgerInfoResponseMessage = new Message(
+  mockLedgerInfoResponseContent,
+)
+
+export const mockSymbolBalance = [tag(10000, Identity1), 1000000]
+export const mockSymbolBalance2 = [tag(10000, Identity2), 5000000]
+
+export const mockLedgerBalanceResponseContent = new Map([
+  [
+    4,
+    cbor.encode(
+      new Map([
+        [
+          0,
+          // @ts-ignore
+          new Map([mockSymbolBalance, mockSymbolBalance2]),
+        ],
+      ]),
+    ),
+  ],
+])
+export const mockLedgerBalanceResponseMessage = new Message(
+  mockLedgerBalanceResponseContent,
+)
+export const expectedBalancesMap = {
+  balances: new Map([
+    [identityStr1, 1000000],
+    [identityStr2, 5000000],
   ]),
 }
