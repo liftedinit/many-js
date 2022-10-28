07e37b99dae52fe1fdd8e7544be25752583c3d28
Tommy <tommy@theliftedinitiative.org>
2022-10-28 09:44:49 -0700

    move enum export

--- src/network/modules/blockchain/index.ts
+++ src/network/modules/blockchain/index.ts
@@ -1,10 +1,13 @@
-export { Blockchain } from "./blockchain"
+export {
+  Blockchain,
+  RangeBlockQueryType,
+  SingleBlockQueryType,
+} from "./blockchain"
+
 export type {
   InfoReturns,
   BlockReturns,
   TransactionReturns,
   BlockListReturns,
   BlockIdentifier,
-  RangeBlockQueryType,
-  SingleBlockQueryType,
 } from "./blockchain"
