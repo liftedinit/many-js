785e161716227ff71c4a8efaf133252a7d17acdf
Stanley Jones <stanley@sunshocked.com>
2022-12-15 13:34:16 -0800

    fix: Make services importable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./identity"
 export * from "./message"
 export * from "./network"
+
+export { Ledger as LedgerService, KeyValue as KeyValueService } from "./server"
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,4 @@
 export { Server } from "./server"
 export { Base as BaseServer } from "./base"
+export { KeyValue } from "./keyvalue"
+export { Ledger } from "./ledger"
