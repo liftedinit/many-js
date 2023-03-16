a971d75257c6e578805e281fa0a856cdb544fecc
Stanley Jones <stanley@sunshocked.com>
2023-03-16 09:25:10 -0700

    fix: Dynamically require crypto lib only in Node environments

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,8 +1,6 @@
-import * as nodeCrypto from "crypto";
-
 export function makeRandomBytes(size = 32) {
-  if (typeof globalThis.crypto === "undefined") {
-    return nodeCrypto.randomBytes(size);
+  if (globalThis?.crypto) {
+    return crypto.getRandomValues(new Uint8Array(size));
   }
-  return crypto.getRandomValues(new Uint8Array(size));
+  return require("crypto").randomBytes(size);
 }
