53f6700bf4f74c667120fd42039bfc262d0a6600
Stanley Jones <stanley@sunshocked.com>
2023-03-16 12:53:17 -0700

    refactor: Utility functions for Buffer toString and from

--- src/shared/__tests__/utils.test.ts
+++ src/shared/__tests__/utils.test.ts
@@ -2,6 +2,10 @@ import { makeRandomBytes, fromString, toString } from "../utils";
 
 describe("Utils", () => {
   describe("makeRandomBytes", () => {
+    it("should return a Uint8Array", () => {
+      const bytes = makeRandomBytes();
+      expect(bytes instanceof Uint8Array).toBe(true);
+    });
     it("should not make the same bytes each time", () => {
       expect(makeRandomBytes()).not.toStrictEqual(makeRandomBytes());
     });
