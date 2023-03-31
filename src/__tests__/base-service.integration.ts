fffd4eb51d507085b64afe61c5f57520e1d8a079
Stanley Jones <stanley@sunshocked.com>
2023-03-31 11:07:35 -0700

    Replicate v1 tests

--- src/__tests__/base-service.integration.ts
+++ src/__tests__/base-service.integration.ts
@@ -1,43 +1,47 @@
 import { BaseService, Request } from "..";
-import { fromString } from "../shared/utils";
 import { ID1, SERVERS } from "./data";
 
 describe("BaseService", () => {
-  const HEX = "D28440a053d92711a2036673746174757305c11a64234ee740";
-  const CBOR_DATA = fromString(HEX, "hex");
+  const HEX = "d28440a053d92711a2036673746174757305c11a64234ee740";
+  const CBOR_DATA = Buffer.from(HEX, "hex");
 
-  const server = new BaseService(SERVERS.LEDGER);
+  let server: BaseService;
 
   it("should send encoded bytes", async () => {
+    server = new BaseService(SERVERS.LEDGER);
     const status = await server.sendEncoded(CBOR_DATA);
 
     expect(status).toBeDefined();
   });
   it("should send a request from bytes", async () => {
+    server = new BaseService(SERVERS.LEDGER);
     const req = Request.fromCborData(CBOR_DATA);
     const status = await server.send(req);
 
     expect(status).toBeDefined();
   });
   it("should send a request from an object", async () => {
+    server = new BaseService(SERVERS.LEDGER);
     const req = Request.fromObject({ method: "status" });
     const status = await server.send(req);
 
     expect(status).toBeDefined();
   });
   it("should call a method", async () => {
+    server = new BaseService(SERVERS.LEDGER);
     const status = await server.call("status");
 
     expect(status).toBeDefined();
   });
   it("should call a method from a shortcut", async () => {
+    server = new BaseService(SERVERS.LEDGER);
     const status = await server.status();
 
     expect(status).toBeDefined();
   });
   it("should call a method non-anonymously", async () => {
-    const id1Server = new BaseService(SERVERS.LEDGER, ID1);
-    const status = await id1Server.status();
+    server = new BaseService(SERVERS.LEDGER, ID1);
+    const status = await server.status();
 
     expect(status).toBeDefined();
   });
